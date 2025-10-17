2a816e7602aec886ab238f8a6906dec7dffcd906
Widdef <mzygowski@hotmail.com>
2025-10-17 07:10:20 +0200

    Working Hello Word configuration - added auto open window - fixed configuration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Builder;
+using System.Diagnostics;
+
+var options = new WebApplicationOptions
+{
+    Args = args,
+    WebRootPath = Path.Combine(Directory.GetCurrentDirectory(), "ClientApp", "dist")
+};
+
+var builder = WebApplication.CreateBuilder(options);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -11,31 +20,27 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
+else
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    app.UseHttpsRedirection();
 }
+
+app.UseDefaultFiles(); // Enables default file mapping (e.g., index.html)
+app.UseStaticFiles();
+
+app.Lifetime.ApplicationStarted.Register(() =>
+{
+    try
+    {
+        var url = app.Urls.FirstOrDefault() ?? "http://localhost:5272";
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = url,
+            UseShellExecute = true
+        });
+    }
+    catch { /* ignore */ }
+});
+
+app.MapFallbackToFile("/index.html");
+app.Run();
